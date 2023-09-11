0ec60a10830ebd620dd6a864a5aae1353c2a3f61
Mostafa <Mostafagholipore@gmail.com>
2023-09-11 11:52:20 +0330

    edite addExpertToSubService

--- src/main/java/com/example/faz3/dto/addExpertToSubService.java
+++ src/main/java/com/example/faz3/dto/addExpertToSubService.java
@@ -7,7 +7,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class addExpertToSubService {
+public class addExpertToSubService  {
    String titleSubService;
    String expertUsername;
 }
