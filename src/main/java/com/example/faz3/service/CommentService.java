ea60ae3b63b6d8355aa9973c0bd2539a7c4d3825
Mostafa <Mostafagholipore@gmail.com>
2023-09-15 20:41:10 +0330

    creat CommentService.java

--- src/main/java/com/example/faz3/service/CommentService.java
+++ src/main/java/com/example/faz3/service/CommentService.java
@@ -1,21 +1,7 @@
 package com.example.faz3.service;
 
 import com.example.faz3.entity.Comment;
-import com.example.faz3.repository.CommentRepository;
-import jakarta.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor
-@Service
-public class CommentService {
-    private final CommentRepository repository;
-//@Autowired
-//    public CommentService(CommentRepository repository) {
-//        this.repository = repository;
-//    }
-@Transactional
-    public void save(Comment comment){
-        repository.save(comment);
-    }
+public interface CommentService {
+    void save(Comment comment);
 }
